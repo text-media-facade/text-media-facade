4898f836f197a62578b27534be32c6e35752f497
hyunjin-C <zn9852@naver.com>
2023-10-24 14:34:50 +0900

    :art: 프로젝트 구조 및 라우터 셋팅

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Main from "./pages/main";
+import Login from "./pages/login";
+import Display from "./pages/display";
+import Common from "./pages/common";
+import Dev from "./pages/dev";
+import Guest from "./pages/guest";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/main" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/display" element={<Display />} />
+        <Route path="/common" element={<Common />} />
+        <Route path="/dev" element={<Dev />} />
+        <Route path="/guest" element={<Guest />} />
+      </Routes>
+    </Router>
   );
 }
 
