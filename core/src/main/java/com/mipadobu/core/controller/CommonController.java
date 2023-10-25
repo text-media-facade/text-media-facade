45aa470dec2914149fc4d80d3d1e6aa32b735614
yih5025 <yih5025@naver.com>
2023-10-25 11:38:59 +0900

    10/25 common page&display connection & making preset logic

--- core/src/main/java/com/mipadobu/core/controller/CommonController.java
+++ core/src/main/java/com/mipadobu/core/controller/CommonController.java
@@ -1,4 +1,34 @@
 package com.mipadobu.core.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
+@Controller
 public class CommonController {
+
+    @GetMapping("/")
+    public String commonGet() {
+        return "common";
+    }
+    @GetMapping("/commonToDisplay")
+    public String commonToDisplay(@RequestParam("text") String text, @RequestParam("preset") String preset, Model model) throws UnsupportedEncodingException {
+        String decodedText = URLDecoder.decode(text, "UTF-8");
+        model.addAttribute("preset", preset);
+        model.addAttribute("text", decodedText);
+        return "display";
+    }
+
+    @PostMapping("/common-post")
+    public String commonPost(@RequestParam("text") String text, @RequestParam("preset") String preset, Model model) throws UnsupportedEncodingException {
+        String encodedText = URLEncoder.encode(text, "UTF-8");
+        return "redirect:/commonToDisplay?text=" + encodedText + "&preset=" + preset;
+    }
 }
