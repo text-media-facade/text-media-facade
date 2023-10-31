fd72996b64fe2cbd12ae29bc653bea5e69834f57
hyunjin-C <zn9852@naver.com>
2023-10-31 23:59:58 +0900

    :loud_sound: 변경사항 업데이트

--- src/pages/common/common.jsx
+++ src/pages/common/common.jsx
@@ -1,71 +1,102 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import {
-    MainContainer,
-    Header,
-    TextInput,
-    Text,
-    Input1,
-    MyFunction,
-    DecoList,
-    Button,
-    T,
-    Back,
-    Deco,
-    Button1,
-    Button2,
-    Button3,
-    Button4
-} from './style';
-import { Link } from 'react-router-dom';
+  MainContainer,
+  Header,
+  TextInput,
+  Text,
+  Input1,
+  MyFunction,
+  DecoList,
+  Button,
+  T,
+  Back,
+  Deco,
+  Button1,
+  Button2,
+  Button3,
+  Button4,
+} from "./style";
+import { Link } from "react-router-dom";
 // import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 function CommonPage() {
+  const [text, setText] = useState("");
 
-    const [text, setText] = useState('');
+  const resetInput = () => {
+    setText("");
+  };
 
-    const resetInput = () => {
-        setText('');
-    };
+  const [isClicked, setIsClicked] = useState(false);
 
-    const [isClicked, setIsClicked] = useState(false);
-
-    const handleButtonClick = () => {
-        if (!isClicked) {
-            setIsClicked(true);
+  const handleButtonClick = () => {
+    if (!isClicked) {
+      setIsClicked(true);
     }
-    };
+  };
 
-    return (
-        <MainContainer>
-            <Header>
-                <Link to="/main"><Back/></Link>
-                <h2>텍스트 입력 일반 사용자</h2>
-            </Header>
-            <TextInput>
-                <T/>
-                <Text>텍스트</Text>
-                <Input1 
-                  placeholder="텍스트를 입력해주세요." 
-                  type="text"
-                  value={text}
-                  onChange={(e) => setText(e.target.value)}/>
-            </TextInput>
-            <MyFunction>
-                <div>
-                    <Deco/>
-                    <Text>텍스트 꾸밈 리스트</Text>
-                </div>
-                <DecoList>
-                    <li><Button1 className={isClicked ? 'clicked' : ''} onClick={handleButtonClick}>효과1</Button1></li>
-                    <li><Button2 className={isClicked ? 'clicked' : ''} onClick={handleButtonClick}>효과2</Button2></li>
-                    <li><Button3 className={isClicked ? 'clicked' : ''} onClick={handleButtonClick}>효과3</Button3></li>
-                    <li><Button4 className={isClicked ? 'clicked' : ''} onClick={handleButtonClick}>효과4</Button4></li>            
-                </DecoList>
-                <Button type="submit" onClick={resetInput}>제출</Button>
-            </MyFunction>
-        </MainContainer>
-    );
+  return (
+    <MainContainer>
+      <Header>
+        <Link to="/main">
+          <Back />
+        </Link>
+        <h2>텍스트 입력 일반 사용자</h2>
+      </Header>
+      <TextInput>
+        <T />
+        <Text>텍스트</Text>
+        <Input1
+          placeholder="텍스트를 입력해주세요."
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+      </TextInput>
+      <MyFunction>
+        <div>
+          <Deco />
+          <Text>텍스트 꾸밈 리스트</Text>
+        </div>
+        <DecoList>
+          <li>
+            <Button1
+              className={isClicked ? "clicked" : ""}
+              onClick={handleButtonClick}
+            >
+              효과1
+            </Button1>
+          </li>
+          <li>
+            <Button2
+              className={isClicked ? "clicked" : ""}
+              onClick={handleButtonClick}
+            >
+              효과2
+            </Button2>
+          </li>
+          <li>
+            <Button3
+              className={isClicked ? "clicked" : ""}
+              onClick={handleButtonClick}
+            >
+              효과3
+            </Button3>
+          </li>
+          <li>
+            <Button4
+              className={isClicked ? "clicked" : ""}
+              onClick={handleButtonClick}
+            >
+              효과4
+            </Button4>
+          </li>
+        </DecoList>
+        <Button type="submit" onClick={resetInput}>
+          제출
+        </Button>
+      </MyFunction>
+    </MainContainer>
+  );
 }
 
 export default CommonPage;
